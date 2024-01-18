e01c173e26534ef403e3edb2077b489bbb90bc8f
hermann-osi <107543367+hermann-osi@users.noreply.github.com>
2024-01-18 15:14:38 +0100

    Update check4kTranscode.sh
    Fixed echo in the wrong place

--- check4kTranscode.sh
+++ check4kTranscode.sh
@@ -16,13 +16,12 @@ data = json.load(sys.stdin)
 count = sum(1 for session in data['response']['data']['sessions'] if session['video_resolution'] == '4k' and session['transcode_decision'] == 'transcode')
 print(count)
 ")
-    echo "$transcode_count stream 4K currently playing"
     echo $transcode_count
 }
 
 # Main logic
 current_transcodes=$(get_actual_4k_transcode_count)
-
+echo "$current_transcodes stream 4K currently playing"
 if [ "$current_transcodes" -ge 2 ]; then
     echo "A 4K transcode is already in progress. Calling killscript"
     python $killscript_path "$@"
